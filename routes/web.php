60df18b0fcd631c2dbb31e05bbe019044b0a72b6
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-01 18:19:52 +0600

    Prepare to CRUD categories

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,12 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
     });
+
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController');
+    });
 });
+
 
 Auth::routes();
 
