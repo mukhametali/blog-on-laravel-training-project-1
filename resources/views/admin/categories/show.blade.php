6ef8da8fe9f466b4c8033222c70a991cc39d6a16
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-03 10:49:31 +0600

    Delete categories

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,7 +8,16 @@
                 <div class="row mb-2">
                     <div class="col-sm-6 d-flex align-items-center">
                         <h1 class="m-0 mr-2">{{ $category->title }}</h1>
-                        <td><a href="{{ route('admin.category.edit', $category->id) }}" class="text-success"><i class="fas fa-pencil-alt"></i></a></td>
+                        <td><a href="{{ route('admin.category.edit', $category->id) }}" class="text-success"><i
+                                    class="fas fa-pencil-alt"></i></a></td>
+                        <form action="{{ route('admin.category.delete', $category->id) }}"
+                              method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="border-0 bg-transparent">
+                                <i class="fas fa-trash text-danger" role="button"></i>
+                            </button>
+                        </form>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -36,7 +45,7 @@
                                         <td>ID</td>
                                         <td>{{ $category->id }}</td>
                                     </tr>
-                                     <tr>
+                                    <tr>
                                         <td>Название</td>
                                         <td>{{ $category->title }}</td>
                                     </tr>
