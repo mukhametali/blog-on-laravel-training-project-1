a397be6ebdf066d0d69295d608beaae2ded2c2f8
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-05 11:47:15 +0600

    Email verification

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
 use App\Mail\User\PasswordMail;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
@@ -17,7 +18,8 @@ class StoreController extends Controller
         $data = $request->validated();
         $password = Str::random(10);
         $data['password'] = Hash::make($password);
-        User::firstOrCreate(['email' => $data['email']], $data);
+        $user = User::firstOrCreate(['email' => $data['email']], $data);
+        event(new Registered($user));
         Mail::to($data['email'])->send(new PasswordMail($password));
         return redirect()->route('admin.user.index');
     }
