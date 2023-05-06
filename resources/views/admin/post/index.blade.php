a468716ad2c403da2d47761ed4a141bef6eb85f8
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-06 15:20:36 +0600

    Frontend customizing and redirect

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -7,12 +7,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Пост</h1>
+                        <h1 class="m-0">Посты</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin.main.index') }}">Главная</a></li>
+                            <li class="breadcrumb-item active">Посты</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
