5f5e4c3f8fd4d7c47d1849b609891bc6f2e7790c
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-06 16:13:07 +0600

    The structure in the database for likes and displaying them in your account

--- app/Http/Controllers/Personal/Liked/IndexController.php
+++ app/Http/Controllers/Personal/Liked/IndexController.php
@@ -8,7 +8,8 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
+        $posts = auth()->user()->likedPosts;
 
-        return view('personal.comment.index');
+        return view('personal.liked.index', compact('posts'));
     }
 }
